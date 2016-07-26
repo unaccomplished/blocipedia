d0230f9cadbec1a363aaa418b1fbfc569ea554be
Jocelyn Hsu <hsu.joce@gmail.com>
2016-07-26 00:44:56 +0000

    Fix tests for Devise, now all pass

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe WelcomeController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
   describe "GET #about" do
     it "returns http success" do
       get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to redirect_to(new_user_session_path)
     end
   end
 
