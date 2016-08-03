64733de2110c9f587edff7dbf1fbafe86d3cf563
Jocelyn Hsu <jocelyn@Jocelyns-iMac.local>
2016-08-03 11:20:25 -0700

    Work in Progress User Roles

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,12 +1,9 @@
 class WikiPolicy < ApplicationPolicy
     attr_reader :user, :wiki
-    
+
     def initialize(user, wiki)
         @user = user
         @wiki = wiki
     end
     
-    def update?
-        user.present?
-    end
 end
