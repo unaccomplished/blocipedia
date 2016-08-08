f29841b8f14efc2c980b4848acb59d737f10fa3c
Jocelyn Hsu <jocelyn@Jocelyns-iMac.local>
2016-08-08 13:59:18 -0700

    Add user roles, no tests written

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :wikis
+  after_initialize :check_role
+
+  enum role: [:standard, :premium, :admin]
+
+  def check_role
+    self.role ||= :standard
+  end
 end
--- spec/policies/wikipolicies_spec.rb
+++ spec/policies/wikipolicies_spec.rb
@@ -1,11 +1,11 @@
 # require 'rails_helper'
 #
 # RSpec.describe WikiPolicy do
-#   let(:wiki) { create(:wiki) }
+#   subject { described_class}
 #
 #   permissions :update?, :edit? do
 #     it "grants access to user to update if user is present" do
-#       expect(:wiki).to permit(User, Wiki.update)
+#       expect(subject).to permit(User.new, Wiki.update)
 #     end
 #   end
 # end
