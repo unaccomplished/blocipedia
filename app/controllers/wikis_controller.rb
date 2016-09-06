11b1870914b9d53988b73af42db3240904699ec6
Jocelyn Hsu <jocelyn@Jocelyns-iMac.local>
2016-09-05 20:59:57 -0700

    Fix Standard User Collaborator seeing the collaboration edit form, fix standard user edit private wiki

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -35,7 +35,7 @@ class WikisController < ApplicationController
     @wiki = Wiki.find(params[:id])
     @wiki.title = wiki_params[:title]
     @wiki.body = wiki_params[:body]
-    @wiki.private = wiki_params[:private]
+    @wiki.private = wiki_params[:private] #|| true # true for standard user/collaborator updating wiki
     authorize @wiki
 
     if @wiki.update(wiki_params)
